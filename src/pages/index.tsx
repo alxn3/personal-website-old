0b4f516e2a9e0eb23f085b3c9b1bbc335f5db822
Alex Lin <alexlin247@gmail.com>
2021-05-04 16:58:02 -0400

    Format files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-8xl">Hello There.</h1>
     </div>
-  )
+  );
 }
