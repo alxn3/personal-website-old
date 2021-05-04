acdcd6a6ac48282e31ec5164179828ccfbc69957
Alex Lin <alexlin247@gmail.com>
2021-05-04 16:53:45 -0400

    Create src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1>Hello There.</h1>
+      <h1 className="text-8xl">Hello There.</h1>
     </div>
   )
 }
