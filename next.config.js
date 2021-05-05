030a73394be697b8e30522607a8060cb7224b691
Alex Lin <alexlin247@gmail.com>
2021-05-04 22:25:18 -0400

    Add wasm webpack plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const path = require('path');
+
 module.exports = {
   webpack(config) {
     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    config.plugins.push(
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, 'wasm'),
+      })
+    );
+
     return config;
   },
 };
