101ff0f1c5eae5b8b24d49f813ab20ea72c6be38
Alex Lin <alexlin247@gmail.com>
2021-05-05 00:59:27 -0400

    Add canvas and test console

--- src/pages/fluid.tsx
+++ src/pages/fluid.tsx
@@ -1,8 +1,52 @@
-
+import { useEffect, useRef } from 'react';
 
 const FluidSimulation = () => {
-  import('wasm/pkg').then((module) => console.log(module.fib(10)));
-  return <div></div>;
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    import('wasm/pkg').then((module) => {
+      const canvas: HTMLCanvasElement = canvasRef.current;
+      const gl = canvas.getContext('webgl', { antialias: true });
+
+      gl.enable(gl.BLEND);
+      gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+      const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
+      let lastDrawTime = -1; // milliseconds
+
+      let requestId;
+
+      module.HELLO()
+      const render = () => {
+        requestId = requestAnimationFrame(render);
+        const currTime = Date.now();
+
+        if (currTime >= lastDrawTime + FPS_THROTTLE) {
+          lastDrawTime = currTime;
+
+          if (window.innerHeight !== canvas.height || window.innerWidth !== canvas.width) {
+            canvas.height = window.innerHeight;
+            // canvas.style.height = window.innerHeight;
+
+            canvas.width = window.innerWidth;
+            // canvas.style.width = window.innerWidth;
+
+            gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+        }
+        }
+      };
+      render();
+
+      return () => {
+        cancelAnimationFrame(requestId);
+      };
+    });
+  }, []);
+  return (
+    <div>
+      <canvas ref={canvasRef} />
+    </div>
+  );
 };
 
 export default FluidSimulation;
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,12 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
 pub fn add_two_ints(a: u32, b: u32) -> u32 {
    a + b
 }
@@ -11,4 +17,9 @@ pub fn fib(n: u32) -> u32 {
    }
 
    fib(n - 1) + fib(n - 2)
+}
+
+#[wasm_bindgen]
+pub fn HELLO() {
+  log("HELLO!")
 }
